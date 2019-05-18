3bda61d7050a71fb2710ed612f2ca9b1bbc27f15
lexuzel <oleksorlov96@gmail.com>
2019-05-18 09:58:49 +0300

    race condition problem solving added

---
+++ Mutex-control/Mutex-control.cpp
@@ -0,0 +1,31 @@
+﻿// Mutex-control.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+void func(std::mutex &m) {
+	std::lock_guard<std::mutex> guard(m);
+	std::thread::id id = std::this_thread::get_id();
+	std::cout << "From thread " << id << std::endl;
+}
+
+int main(){
+	auto loop = [](std::mutex &m) {
+		for (int i = 0; i < 10; i++) func(m);
+	};
+
+	std::mutex my_mutex;
+
+	std::thread t1(loop, std::ref(my_mutex));
+	std::thread t2(loop, std::ref(my_mutex));
+	std::thread t3(loop, std::ref(my_mutex));
+
+	t1.join();
+	t2.join();
+	t3.join();
+
+}
+

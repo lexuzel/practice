abf2277d72f41613de74b59538949716a5482c78
lexuzel <oleksorlov96@gmail.com>
2019-05-26 22:09:44 +0300

    features matching added

---
+++ FlannFeatureMatcher/source.cpp
@@ -0,0 +1,67 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+
+using namespace cv;
+
+Mat img_1, img_2, dst;
+std::string windowname{ "Features" };
+int max_coefficient{ 20 };
+
+void match(int, void*) {
+	int minHessian = 400;
+	auto detector = xfeatures2d::SURF::create(minHessian);
+	std::vector<KeyPoint> keypoints_1, keypoints_2;
+	Mat descriptors_1, descriptors_2;
+	detector->detectAndCompute(img_1, Mat(), keypoints_1, descriptors_1);
+	detector->detectAndCompute(img_2, Mat(), keypoints_2, descriptors_2);
+	//-- Step 2: Matching descriptor vectors using FLANN matcher
+	FlannBasedMatcher matcher;
+	std::vector< DMatch > matches;
+	matcher.match(descriptors_1, descriptors_2, matches);
+	double max_dist = 0; double min_dist = 100;
+	//-- Quick calculation of max and min distances between keypoints
+	for (int i = 0; i < descriptors_1.rows; i++)
+	{
+		double dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
+	}
+	//-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+	//-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+	//-- small)
+	//-- PS.- radiusMatch can also be used here.
+	std::vector< DMatch > good_matches;
+	for (int i = 0; i < descriptors_1.rows; i++)
+	{
+		if (matches[i].distance <= max((max_coefficient * min_dist) / 10, 0.02))
+		{
+			good_matches.push_back(matches[i]);
+		}
+	}
+	//-- Draw only "good" matches
+	Mat img_matches;
+	drawMatches(img_1, keypoints_1, img_2, keypoints_2,
+		good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+		std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+	//-- Show detected matches
+	imshow("Good Matches", img_matches);
+}
+
+int main() {
+	img_1 = imread("..\\images\\42.jpg", IMREAD_COLOR);
+	img_2 = imread("..\\images\\templ_02.jpg", IMREAD_COLOR);
+	if (!img_1.data || !img_2.data)
+	{
+		std::cout << " --(!) Error reading images " << std::endl; return -1;
+	}
+	namedWindow(windowname, WINDOW_AUTOSIZE);
+	createTrackbar("Max coefficient: ", windowname, &max_coefficient, 100, match);
+
+	waitKey();
+	destroyAllWindows();
+	return 0;
+}
---
+++ Image-Features/source.cpp
@@ -0,0 +1,35 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+using namespace cv;
+
+int main(){
+	Mat img_1 = imread("..\\images\\42.jpg", IMREAD_COLOR);
+	Mat img_2 = imread("..\\images\\templ_02.jpg", IMREAD_COLOR);
+	if (!img_1.data || !img_2.data)
+	{
+		std::cout << " --(!) Error reading images " << std::endl; return -1;
+	}
+	//-- Step 1: Detect the keypoints using SURF Detector
+	int minHessian = 6000;
+	auto detector = xfeatures2d::SURF::create(minHessian);
+	std::vector<KeyPoint> keypoints_1, keypoints_2;
+	Mat descriptors_1, descriptors_2;
+	detector->detectAndCompute(img_1, Mat(), keypoints_1, descriptors_1);
+	detector->detectAndCompute(img_2, Mat(), keypoints_2, descriptors_2);
+	//-- Step 2: Matching descriptor vectors with a brute force matcher
+	BFMatcher matcher(NORM_L2);
+	std::vector< DMatch > matches;
+	matcher.match(descriptors_1, descriptors_2, matches);
+	//-- Draw matches
+	Mat img_matches;
+	drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_matches);
+	//-- Show detected matches
+	imshow("Matches", img_matches);
+	waitKey(0);
+	return 0;
+}
+
